8b018fa642b90114aad9bc1dd8bea60ec78421ef
zeeshan <zeehaancode21@gmail.com>
2025-07-14 01:24:21 +0530

    Resolved vue.config.js merge conflict

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-<<<<<<< HEAD
   publicPath: '/gym/',
-=======
-   publicPath: '/gym/',
->>>>>>> be4b2712f6e239f667dae17ebb644aaf046a1b11
   transpileDependencies: true
 })
